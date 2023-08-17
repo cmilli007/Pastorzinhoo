40e014bb615f8d8e498b18d9f1dc1405d1d312df
camili.costa07@gmail.com <e107ti@etec.sp.gov.br>
2023-08-17 14:43:19 -0300

    adicionar

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("DÓ, "); Console.Beep(1320,300);
+Console.WriteLine("RÉ,"); Console.Beep(1485,300);
+Console.WriteLine("MI,");Console.Beep(1650,300);
+Console.WriteLine("FÁ,");Console.Beep(1759,300);
+Console.WriteLine("FÁ,");Console.Beep(1759,300);
+Console.WriteLine("FÁ,");Console.Beep(1759,300);
+Thread.Sleep(5000);
+Console.WriteLine("DÓ, "); Console.Beep(1320,300);
+Console.WriteLine("RÉ,"); Console.Beep(1485,300);
+Console.WriteLine("DÓ, "); Console.Beep(1320,300);
+Console.WriteLine("RÉ,"); Console.Beep(1485,300);
+Console.WriteLine("RÉ,"); Console.Beep(1485,300);
+Thread.Sleep(3000);
+Console.WriteLine("DÓ, "); Console.Beep(1320,300);
+Console.WriteLine("SOL, "); Console.Beep(1980,300);
+Console.WriteLine("FÁ,");Console.Beep(1759,300);
+Console.WriteLine("MI,");Console.Beep(1650,300);
+Console.WriteLine("MI,");Console.Beep(1650,300);
+Console.WriteLine("MI,");Console.Beep(1650,300);
+Thread.Sleep(3000);
+Console.WriteLine("DÓ, "); Console.Beep(1320,300);
+Console.WriteLine("RÉ,"); Console.Beep(1485,300);
+Console.WriteLine("MI,");Console.Beep(1650,300);
+Console.WriteLine("FÁ,");Console.Beep(1759,300);
+Console.WriteLine("FÁ,");Console.Beep(1759,300);
+Console.WriteLine("FÁ,");Console.Beep(1759,300);
